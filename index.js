5a4fbeb1797a3acdc34b06ef156d5d148a0787ce
Bishnu <thapabishnu20@gmail.com>
2025-12-17 14:20:57 +0545

    nodemon & dotenv for loading .env variables for dynamic port

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, Bishnu!");
 });
 
-app.get('/api/courses', (req, res) => {
+app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3, 4, 5]);
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...   `);
 });
