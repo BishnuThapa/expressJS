c8e70a723077c90d63565344dcf6a8c106205641
Bishnu <thapabishnu20@gmail.com>
2025-12-17 14:52:31 +0545

    handle http post request

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const courses = [
   { id: 1, name: "course1" },
   { id: 2, name: "course2" },
@@ -15,6 +17,15 @@ app.get("/", (req, res) => {
 
 app.get("/api/courses", (req, res) => {
   res.send(courses);
+});
+
+app.post("/api/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  }
+  courses.push(course);
+  res.send(course);
 });
 
 app.get('/api/courses/:id', (req, res) => {
